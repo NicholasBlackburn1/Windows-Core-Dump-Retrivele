9251007b34ef34c35a7d42ed0d39dfbbfe1aa29e
nickyBlackBurn <nickblackburn02@gmail.com>
2021-09-15 09:37:23 -0400

    added messaing.py and added stuff to main.py

--- CoreDumpRetreval/__main__.py
+++ CoreDumpRetreval/__main__.py
@@ -1,3 +1,37 @@
 """
 Main file for the module this is where code will execute
 """
+import imports 
+
+
+
+
+
+
+
+
+## this is the main function this will run the module when u rn py CoredumpRetrievals
+def Main():
+    imports.logging.basicConfig(filename= str(imports.datetime.now())+'.log', encoding='utf-8', level=imports.logging.DEBUG)
+    
+    imports.logging.info("Starting Up the CoreDumpRetervil")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__== '__main__':
+    Main()
--- CoreDumpRetreval/imports.py
+++ CoreDumpRetreval/imports.py
@@ -1,3 +1,6 @@
 """
 this is where put the imports for the module are at
 """
+
+import logging
+from datetime import datetime
