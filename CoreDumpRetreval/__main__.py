82d9b8e32a39906398b246c8b62f8fefb21f8c17
nickyBlackBurn <nickblackburn02@gmail.com>
2021-09-15 12:05:42 -0400

    added reading from config

--- CoreDumpRetreval/__main__.py
+++ CoreDumpRetreval/__main__.py
@@ -5,20 +5,35 @@ import imports
 
 
 
-
-
-
-
-
 ## this is the main function this will run the module when u rn py CoredumpRetrievals
-def Main():
-    imports.logging.basicConfig(filename= str(imports.datetime.now().strftime('CoreDumpCollector%Y-%m-%d_%H-%M-%s.log')),level=imports.logging.DEBUG)
 
+def Main():
+    #imports.logging.basicConfig(filename= str(imports.datetime.now().strftime('CoreDumpCollector%Y-%m-%d_%H-%M-%s.log')),level=imports.logging.DEBUG)
+   
     print("starting up Core Dump Collector...")
-    imports.logging.info("Starting Up the CoreDumpCollector")
+
+
+
+    if(imports.platform.system()== 'Windows'):  
+        print('Running on windows... Now going to do windows stuff With windows paths')
+        
+        print("Config file:"+str(imports.pathlib.Path.cwd())+"\Config.ini")
+        
+        config = imports.configparser.ConfigParser()
+        config.read(str(imports.pathlib.Path.cwd())+"\Config.ini"))
+
 
 
     
+    if(imports.platform.system()== 'Linux'):  
+    
+        print('Running on Linux... Now going to do windows stuff With Linux paths')
+        
+        print("Config file:"+str(imports.pathlib.Path.cwd())+"/Config.ini")
+
+        # this will read the config.ini
+        config = imports.configparser.ConfigParser())
+        config.read((str(imports.pathlib.Path.cwd())+"\Config.ini")
 
 
 
--- CoreDumpRetreval/imports.py
+++ CoreDumpRetreval/imports.py
@@ -4,3 +4,10 @@ this is where put the imports for the module are at
 
 import logging
 from datetime import datetime
+import smtplib, ssl
+
+import configparser
+import pathlib
+import platform
+import sys
+from io import StringIO
