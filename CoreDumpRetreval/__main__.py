074989a818f492fb3a931329db7bb7d03453eb1d
nickyBlackBurn <nickblackburn02@gmail.com>
2021-09-15 12:34:21 -0400

    got config file to work ow on to emailin

--- CoreDumpRetreval/__main__.py
+++ CoreDumpRetreval/__main__.py
@@ -14,27 +14,32 @@ def Main():
 
 
 
+    # Runs on Windows platform
     if(imports.platform.system()== 'Windows'):  
         print('Running on windows... Now going to do windows stuff With windows paths')
         
         print("Config file:"+str(imports.pathlib.Path.cwd())+"\Config.ini")
         
         config = imports.configparser.ConfigParser()
-        config.read(str(imports.pathlib.Path.cwd())+"\Config.ini"))
+        config.read(str(imports.pathlib.Path.cwd())+"\Config.ini")
+        
+        email = config['EMAIL']
 
 
 
-    
+    # Runs on linux platform
     if(imports.platform.system()== 'Linux'):  
     
         print('Running on Linux... Now going to do windows stuff With Linux paths')
         
-        print("Config file:"+str(imports.pathlib.Path.cwd())+"/Config.ini")
+        print("Config file:"+" "+str(imports.pathlib.Path.cwd())+"/Config.ini")
 
         # this will read the config.ini
-        config = imports.configparser.ConfigParser())
-        config.read((str(imports.pathlib.Path.cwd())+"\Config.ini")
-
+        config = imports.configparser.ConfigParser()
+        config.read((str(imports.pathlib.Path.cwd())+"/"+"Config.ini"))
+       
+        email= config['EMAIL']
+     
 
 
 
